7cd7854cc7b51b5d8efc07b1a4c5faacacb211cd
Patrick Berne <55303055+bernep@users.noreply.github.com>
2020-04-03 17:26:08 -0400

    Rudimentary Projects Page
    I've created a basic project page, and I've managed to set up how we can add new projects for the time being (Projects can be added under data() in Project.vue). For now, the page looks very poor, though I need to upload this so that someone else can implement an individual project page template using the project data here.

---
+++ showcase/src/components/Layout/ProjectItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="project-item" v-bind:class="{'info':!project.active}">
+        <h3>
+        <img v-bind:alt="project.title + ' logo'" v-bind:src="require('../../assets/' + project.title.toLowerCase() + '.png')" width = 400>
+            <input type="checkbox" v-on:change="markActive">
+            {{project.title}}
+        </h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProjectItem",
+    props: ["project"],
+    methods: {
+        markActive() {
+            this.project.active = !this.project.active;
+        }
+    },
+}
+</script>
+
+<style scoped>
+.project-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+
+.is-active {
+    text-decoration: line-through;
+}
+
+.info {
+    src: "../../assets/observatory.png";
+}
+</style>
